2ef0e00ebcee980b48ac867fb914244575e62f89
tangsakul <mr.tangsakul@gmail.com>
2026-09-24 09:45:09 +0700

    Init Master

--- src/labgitusing/LabGitUsing.java
+++ src/labgitusing/LabGitUsing.java
@@ -16,8 +16,6 @@ public class LabGitUsing {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Master");
-        System.out.println("Master");
-        System.out.println("Master");
     }
     
 }

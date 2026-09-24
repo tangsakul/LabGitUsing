82e829fb58d9bfeb9be6fedda8f2f726e8efc98d
tangsakul <mr.tangsakul@gmail.com>
2026-09-24 10:40:06 +0700

    Add an Interface.

--- src/labgitusing/LabGitUsing.java
+++ src/labgitusing/LabGitUsing.java
@@ -16,6 +16,8 @@ public class LabGitUsing {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Master");
+        System.out.println("Master");
+        System.out.println("Master");
     }
     
 }
